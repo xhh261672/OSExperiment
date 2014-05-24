7cddfb5571c7fbfb823cf0ab81844a3440c76f44
= <=>
2014-05-24 14:04:54 +0800

    ex1 1st

---
+++ e1.c
@@ -0,0 +1,58 @@
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MAX_SEQUENCE 10
+
+int main(int argc, char const *argv[])
+{
+	typedef struct 
+	{
+		long fib_sequence [MAX_SEQUENCE];
+		int sequence_size;
+	} share_data;
+
+	share_data shdat ;
+	int segment_id = shmget(IPC_PRIVATE,sizeof(shdat),S_IRUSR | S_IWUSR);
+	char *shmaddr = (char*)shmat(segment_id,NULL,0);
+	int sq_size;
+	scanf("%d",sq_size);
+	sprintf(shdat.sequence_size,sq_size);
+	int pid;
+
+	pid = fork();
+
+	if(pid == 0){
+		shdat.fib_sequence[0] = 0;
+		shdat.fib_sequence[1] = 1;
+		int prev = shdat.fib_sequence[0];
+		int succ = shdat.fib_sequence[1];
+		int i;
+		for(i = 2 ;i < shdat.sequence_size;i++){
+			shdat.fib_sequence[i] = prev + succ;
+			prev = succ;
+			succ = shdat.fib_sequence[i];
+		}
+		i = 0;
+		shmdt(shmaddr);
+		return 0;
+
+	}
+
+	else if(pid > 0){
+		wait(NULL);
+		int fib_count;
+		fib_count = shdat.sequence_size;
+		int i;
+		for(i = 0; i< fib_count;i++){
+			printf("%d\t", shdat.fib_sequence[fib_count]);
+		}
+		shmclt(segment_id,IPC_RMID,NULL);
+	}
+
+
+	return 0;
+}
